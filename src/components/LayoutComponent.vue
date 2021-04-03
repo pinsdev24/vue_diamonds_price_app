03bbb59e91dc6b4d2be020a8a62edd419c23507a
pinsdev24 <pinsdev24@gmail.com>
2021-04-03 20:28:36 +0100

    fix prettier pb

--- src/components/LayoutComponent.vue
+++ src/components/LayoutComponent.vue
@@ -2,26 +2,26 @@
   <div>
     <v-navigation-drawer app v-model="drawer" overlay-opacity="">
       <v-list shaped>
-      <v-subheader>ML DIAMONDS</v-subheader>
-      <v-list-item-group
-        v-model="selectedItem"
-        color="primary"
-      >
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-              <router-link :to="item.link" class="text-decoration-none text--grey">
-                <v-list-item-title class="text-uppercase" v-text="item.text"></v-list-item-title>
+        <v-subheader>ML DIAMONDS</v-subheader>
+        <v-list-item-group v-model="selectedItem" color="primary">
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <router-link
+                :to="item.link"
+                class="text-decoration-none text--grey"
+              >
+                <v-list-item-title
+                  class="text-uppercase"
+                  v-text="item.text"
+                ></v-list-item-title>
               </router-link>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </v-navigation-drawer>
     <v-app-bar app color="secondary" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
@@ -40,12 +40,16 @@ export default {
       drawer: false,
       selectedItem: 1,
       items: [
-        { text: 'Home', icon: 'mdi-home', link: "/" },
-        { text: 'About App', icon: 'mdi-file-outline', link: "/about" },
-        { text: 'Our Team', icon: 'mdi-account-multiple-outline', link: "/team" },
-      ],
-    }
-  },
+        { text: "Home", icon: "mdi-home", link: "/" },
+        { text: "About App", icon: "mdi-file-outline", link: "/about" },
+        {
+          text: "Our Team",
+          icon: "mdi-account-multiple-outline",
+          link: "/team"
+        }
+      ]
+    };
+  }
 };
 </script>
 
